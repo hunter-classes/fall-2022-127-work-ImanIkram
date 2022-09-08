da1cdc91b864ec056487184fa9a304d67720b850
Iman Ikram <104866521+ImanIkram@users.noreply.github.com>
2022-09-08 13:41:02 +0000

    triangle function added

--- 127T/square.py
+++ 127T/square.py
@@ -4,21 +4,43 @@ crush = turtle.Turtle()
 
 ## square function 
 
-def square(crush):
+def square(t, x,y, w, color, sidelen):
+  """
+  Draw a square using the turlte passed into t
+  parameters:
+  t -       a turle
+  x,        y - location
+  w -       width
+  color -   color to draw in
+  sidelen - length of each side
+  Returns:
+  nothing
+  """
   
+  ##set the location , color, and width
+  t.penup()
+  t.goto(x, y)
+  t.width(w)
+  t.color(color)
+  t.pendown()
   # draw a square
   for c in range(4):
-    
-    crush.forward(100)
-    crush.left(90)
- 
-crush = turtle.Turtle()
-square(crush)
+    t.forward(100)
+    t.left(90)
 
-squirt = turtle.Turtle()
-squirt.penup()
-squirt.goto(-100, -130)
-squirt.pendown()
-squirt.width(5)
-squirt.color("red")       
-square(squirt)  
+crush = turtle.Turtle()
+square(crush, 0,0, 1, "green", 50)
+
+squirt = turtle.Turtle()       
+square(squirt, -100, -100, 3, "red", 100) 
+
+crush.setheading(45)
+square(crush,150, 20,-100, "blue", 90)   
+     
+
+
+##def hexagon (t, x,y, color, sidelen):
+  ##code to draw the triangle 
+  
+##def ngon (t, numsides, x,y, color, width, sidelen):
+  ##code to draw ngon

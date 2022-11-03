310d3291f108e274228ec0b6d515614742bacc13
Iman Ikram <104866521+ImanIkram@users.noreply.github.com>
2022-11-03 00:23:01 +0000

    modes.py updated with fastMode

--- mode/modeFunction.py
+++ mode/modeFunction.py
@@ -20,8 +20,25 @@ list = [1,2,4,4,6,1,2,3,4,2,4,1,4,2,2,2]
 print("Mode of list -->", modeSoFar(list))
 
 
+# Find Mode Fast
+import random
 
+def buildRandomList(size,maxvalue):
+    result = [random.randrange(maxvalue) for x in range(size)]
+    return result 
 
+dataset = buildRandomList(100,100)
+
+def fastMode(dataset):
+    tallies = [0]*100
+    for item in dataset:
+        tallies[item]+=1 
+    highest = max(tallies)
+    value = tallies.index(highest)
+    return value 
+
+result = fastMode(dataset)
+print("Fast Mode -->", result)
 
 
 
--- mode/modes.py
+++ mode/modes.py
@@ -3,24 +3,13 @@ freq(l,v) which takes a list of numbers (l) and a value (v). The function will r
 of v, that is, the number of times that v appears in l.
 '''
 
-
-
-def findSmallest(s):
-  small = min(s)
-  return small
-
-l1 = [13,34,65,34,67,9,54,32,12,11,10]
-print ("minimum value in l1 -->", findSmallest(l1))
-
-
-# frequency 
+#Find frequency 
 def freq(l,v):
     f = l.count(v)     
     return f
 
 num = [1,2,3,4,8,3,2,8,4,0,3,2,3]
 print ("frequency -->", freq(num,3))
-
 
 #Find Largest 
 def findLargest(l):
@@ -31,11 +20,28 @@ def findLargest(l):
     return large
 
 l2 = [13,34,65,34,67,9,54,32,12,11,10]
-print ("maximum value in l2 -->", findLargest(l2))
+print ("Largest value -->", findLargest(l2))
 
 
+#Find Mode Fast
+import random
 
+def buildRandomList(size,maxvalue):
+    result = [random.randrange(maxvalue) for x in range(size)]
+    return result 
 
+dataset = buildRandomList(100,100)
+
+def fastMode(dataset):
+    tallies = [0]*100
+    for item in dataset:
+        tallies[item]+=1 
+    highest = max(tallies)
+    value = tallies.index(highest)
+    return value 
+
+result = fastMode(dataset)
+print("Fast Mode -->", result)
 
 
 

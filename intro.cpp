44cf72c8721cd7d1ccf0fa6ac1da2511945a938b
Iman Ikram <104866521+ImanIkram@users.noreply.github.com>
2022-11-28 16:14:50 +0000

    intro.cpp added

---
+++ intro.cpp
@@ -0,0 +1,12 @@
+// single line comment 
+
+#include <iostream>
+
+int main() { // need a function named main 
+  std::cout << "Hello World!" << std::endl; 
+  std::cout << "This is a C++ file" << std::endl; 
+  return 0;
+}
+
+// compile: g++ intro.cpp 
+// run: ./a.out

8548e705555660fd89cecdd92010bd20cbb4358e
Iman Ikram <104866521+ImanIkram@users.noreply.github.com>
2022-12-06 19:39:00 +0000

    c++ updated

---
+++ c++ classwork/control.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main ()
+{
+int a
+a = 10;
+if (a<20){
+  std::cout << "a < 20\n";
+  std::cout<< "also in the true part\n";
+}
+
+int b;
+b = 100;  
+if (b > 20) {
+  std::cout << "b > 20";
+} else { 
+  std::cout << "b < 20"; 
+  
+}  
+  
+return 0;   
+}
+
+// && and 
+// ll or
+// ! not 

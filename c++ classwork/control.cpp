02815fd1cc295d7d0df922b6736034ff39a56201
Iman Ikram <104866521+ImanIkram@users.noreply.github.com>
2022-12-08 14:59:31 +0000

    loops.cpp & func.cpp added

--- c++ classwork/control.cpp
+++ c++ classwork/control.cpp
@@ -2,25 +2,26 @@
 
 int main ()
 {
-int a
+int a;
 a = 10;
-if (a<20){
+if (a < 20){
   std::cout << "a < 20\n";
   std::cout<< "also in the true part\n";
 }
 
 int b;
 b = 100;  
-if (b > 20) {
-  std::cout << "b > 20";
+if (b > 20){
+  std::cout << "b > 20\n";
 } else { 
-  std::cout << "b < 20"; 
+  std::cout << "b < 20\n"; 
   
 }  
-  
 return 0;   
 }
 
 // && and 
 // ll or
 // ! not 
+
+
